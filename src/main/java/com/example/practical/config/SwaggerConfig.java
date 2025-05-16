57959b43b96b55c577c2b38354af15e6993778a7
Uchana <uchaneishvili01@gmail.com>
2025-05-16 16:30:51 +0400

    run3

--- src/main/java/com/example/practical/config/SwaggerConfig.java
+++ src/main/java/com/example/practical/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.example.practical.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -13,6 +16,9 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Ecommerce API")
                         .version("1.0")
-                        .description("API Documentation"));
+                        .description("API Documentation"))
+                .servers(List.of(new Server()
+                        .url("/api")
+                        .description("Server URL")));
     }
 }
